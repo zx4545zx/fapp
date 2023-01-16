390b07bf8b30b7246597faaa29fdb186e86f95bb
Natto <zx4545zx.zx4545zx@gmail.com>
2023-01-16 17:25:46 +0700

    update modal on mobile devices

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -35,7 +35,7 @@
     <h2 class="card-title text-xl">0</h2>
     <span>THB</span>
     <div class="card-actions justify-center">
-      <label for="my-modal" class="btn btn-primary">open modal</label>
+      <label for="my-modal-6" class="btn btn-primary">open modal</label>
     </div>
   </div>
 </div>
@@ -43,14 +43,13 @@
 <History limit={10} />
 
 
-<!-- Put this part before </body> tag -->
-<input type="checkbox" id="my-modal" class="modal-toggle" />
-<div class="modal">
+<input type="checkbox" id="my-modal-6" class="modal-toggle" />
+<div class="modal modal-bottom sm:modal-middle">
   <div class="modal-box">
     <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
     <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
     <div class="modal-action">
-      <label for="my-modal" class="btn">Yay!</label>
+      <label for="my-modal-6" class="btn">Yay!</label>
     </div>
   </div>
 </div>
