4e32e46be5ada2a8a4fb89e0864a31dc8730f0c8
Natto <zx4545zx.zx4545zx@gmail.com>
2023-01-16 17:32:48 +0700

    update content model

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -42,12 +42,44 @@
 
 <History limit={10} />
 
-
 <input type="checkbox" id="my-modal-6" class="modal-toggle" />
 <div class="modal modal-bottom sm:modal-middle">
   <div class="modal-box">
     <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
-    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <p class="py-4">
+      You've been selected for a chance to get one year of subscription to use
+      Wikipedia for free!
+    </p>
+
+    <main class="items-center text-center m-auto">
+      <div class="mockup-phone border-primary">
+        <div class="camera" />
+        <div class="display">
+          <div class="artboard artboard-demo phone-1">Hi.</div>
+        </div>
+      </div>
+
+      <div class="stats stats-vertical shadow">
+        <div class="stat">
+          <div class="stat-title">Downloads</div>
+          <div class="stat-value">31K</div>
+          <div class="stat-desc">Jan 1st - Feb 1st</div>
+        </div>
+
+        <div class="stat">
+          <div class="stat-title">New Users</div>
+          <div class="stat-value">4,200</div>
+          <div class="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div class="stat">
+          <div class="stat-title">New Registers</div>
+          <div class="stat-value">1,200</div>
+          <div class="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
+    </main>
+
     <div class="modal-action">
       <label for="my-modal-6" class="btn">Yay!</label>
     </div>
